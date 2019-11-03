9c9ad95e8180f4933dfe6cd1c000202fe264fb21
Hy Nguyen <khanghy2130@gmail.com>
2019-11-03 03:57:24 +0000

    set up to use mongoAtlas

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ var express    = require("express"),
 var campgroundsRoute = require("./routes/campgrounds"),
 	indexRoute = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/campgrounds_app", {useNewUrlParser: true});
+// dev
+// mongoose.connect("mongodb://localhost:27017/campgrounds_app", {useNewUrlParser: true});
+// production https://www.youtube.com/watch?v=iJtOoeM_fS8&feature=youtu.be&t=368
+mongoose.connect("mongodb+srv://dbUser1:0909988855@cluster0-tb0ia.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true}).then(() => {
+	console.log("Connected to DB");
+}).catch(err => {
+	console.log("Failed to connect to DB: " + err.message);
+});
+
 app.set("view engine", "ejs"); // default view file extension
 app.use(bodyParser.urlencoded({extended: true})); // parse form submition
 app.use(express.static(__dirname + '/public')); // public folder
